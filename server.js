ad8fb43cf82bcd46849bc81c3206796af66cd091
Brian Nduhiu <briannduhiu@gmail.com>
2022-02-08 15:28:37 +0300

    created blog

--- server.js
+++ server.js
@@ -1,7 +1,34 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors');
+const { logger } = require('./middleware/logEvents');
 const PORT = process.env.PORT || 3500;
+
+
+app.use(logger);
+
+const whitelist = ['http://www.google.com', 'http://127.0.0.1:5500/', 'http://localhost:3500/'];
+
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true);
+        }
+        else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    optionsSuccessStatus: 200
+};
+
+app.use(cors());
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('^/$|/index(.html)?', (req, res) => {
     //res.sendFile('./views/index.html', { root: __dirname });
@@ -19,7 +46,7 @@ app.get('/old-page(.html)?', (req, res) => {
 // Route handlers
 app.get('/hello(.html)?', (req, res, next) => {
     console.log('attempted to load hello.html');
-    next()
+    next();
 }, (req, res) => {
     res.send('Hello World!');
 });
@@ -29,22 +56,22 @@ app.get('/hello(.html)?', (req, res, next) => {
 const one = (req, res, next) => {
     console.log('one');
     next();
-}
+};
 
 const two = (req, res, next) => {
     console.log('two');
     next();
-}
+};
 
 const three = (req, res) => {
     console.log('three');
     res.send('Finished!');
-}
+};
 
 app.get('/chain(.html)?', [one, two, three]);
 
 app.get('/*', (req, res) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-})
+});
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
